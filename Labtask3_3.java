9e2b83365445eb65f38b6d91564b8506ec630197
wasia902 <117897579+wasia902@users.noreply.github.com>
2022-11-10 13:26:23 +0500

    Create Labtask3_3.java

---
+++ Labtask3_3.java
@@ -0,0 +1,12 @@
+public class labtaskthreepart3{
+    public static void main (String args[]){
+        int a=10;
+        int b=12;
+        System.out.println("The result of a==b = "+(a==b));
+        System.out.println("The result of a!=b = "+(a!=b));
+        System.out.println("The result of a>b = "+(a>b));
+        System.out.println("The result of a<b = "+(a<b));
+        System.out.println("The result of a>=b = "+(a>=b));
+        System.out.println("The result of a<=b = "+(a<=b));
+    }
+}
