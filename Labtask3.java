9f2702f735bf2218e7f6889880d03e033dc3705e
wasia902 <117897579+wasia902@users.noreply.github.com>
2022-11-10 13:24:49 +0500

    Create Labtask3.java

---
+++ Labtask3.java
@@ -0,0 +1,12 @@
+class labtask3{
+    public static void main(String args[]){
+        int a=20;
+        int b=12;
+        System.out.println("addition of a+b :"+(a+b));
+        System.out.println("subtraction of a-b :"+(a-b));
+        System.out.println("multiplication of a*b :"+(a*b));
+        System.out.println("division of a/b : "+(a/b));
+        System.out.println("modulus of a%b :"+(a%b));
+//+ ,-, *, /, % - arithmetic operator
+    }
+}
