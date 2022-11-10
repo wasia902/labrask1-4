2c9e8c2ae3fa5775d0f6ee80578d9b5be7396869
wasia902 <117897579+wasia902@users.noreply.github.com>
2022-11-10 13:23:11 +0500

    Create Labtask1.java

---
+++ Labtask1.java
@@ -0,0 +1,21 @@
+public class labtask1{
+    public static void main(String[] args){
+        int mylnt = 42;
+        byte myByte = 127;
+        short myShort = 32767;
+        long myLong = 992881667654664L;
+        float myFloat = 4.1236545799975f;
+        double myDouble = 5.826355548282;
+        char myChar = 'A';
+        boolean myBoolean = true;
+
+        System.out.println("mylnt:" + mylnt);
+        System.out.println("myByte:"+ myByte);
+        System.out.println("myShort:" + myShort);
+        System.out.println("myLong:" + myLong);
+        System.out.println("myFloat:" + myFloat);
+        System.out.println("myDouble:" + myDouble);
+        System.out.println("myChar:" + myChar);
+        System.out.println("myBoolean:" + myBoolean);
+    }
+}
