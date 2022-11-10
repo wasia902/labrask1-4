23a4fa50a45642312da3460c253f7e58f080a245
wasia902 <117897579+wasia902@users.noreply.github.com>
2022-11-10 13:25:37 +0500

    Create Labtask3_2.java

---
+++ Labtask3_2.java
@@ -0,0 +1,10 @@
+class labtask3{
+    public static void main(String args[]){
+        int a=10;
+        System.out.println("Value of a+=6 is :"+(a+=6));//same as a=a+6;
+        System.out.println("Value of a-=5 is :"+(a-=5));//same as a=a-5;
+        System.out.println("Value of a*=4 is :"+(a*=4));//same as a=a*4;
+        System.out.println("Value of a/=3 is : "+(a/=3));//same as a=a/5;
+        System.out.println("Value of a%=2 is : "+(a%=2));//same as a=a%5;
+//Assignment operator
+    }
